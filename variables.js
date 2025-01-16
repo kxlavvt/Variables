ce28ca2f7c7a68aeccbda4dc39ee73d222d539eb
Clau <kxlaavv@gmail.com>
2025-01-16 18:43:56 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,24 @@
+// 4 variables para el perfil de usuario
+let nombre = "Marcos";
+let edad = "25";
+let email ="marcos@gmail.com";
+let codigo_postal = "28001";
+
+// Imprimir variables del usuario
+console.log("\nPERFIL DE USUARIO");
+console.log("\nNombre:", nombre);
+console.log("Edad:", edad);
+console.log("Correo:", email);
+console.log("Código postal:", codigo_postal);
+
+// Cambiar una variable
+edad = "26";
+
+// Imprimir variables del usuario de nuevo
+console.log("\nPERFIL DE USUARIO ACTUALIZADO");
+console.log("\nNombre:", nombre);
+console.log("Edad:", edad);
+console.log("Correo:", email);
+console.log("Código postal:", codigo_postal);
+
+
